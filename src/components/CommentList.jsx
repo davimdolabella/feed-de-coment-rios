9fa399d943b9ec7b5e9016c2dbeada5b007aa57c
Davi Dolabella <davimdolabella03@gmail.com>
2024-09-15 10:55:39 -0300

    Update CommentList.jsx

--- src/components/CommentList.jsx
+++ src/components/CommentList.jsx
@@ -7,7 +7,7 @@ export default function CommentList({email, comment, date, onRemove}){
             <div className="border border-dark text-dark p-2 rounded mt-2 d-flex flex-column flex-wrap p-3">
                 <h2 className="fs-6 fs-md-4">Comentário:</h2>
                 <p className="text-break " style={{overflowY:"auto", height:"100px"}}>{comment}</p>{/* Comentarios */}
-                <button className="btn btn-outline-danger col-12 fs-6 col-md-6 mx-auto" onClick={() => onRemove}>Remover</button>
+                <button className="btn btn-outline-danger col-12 fs-6 col-md-6 mx-auto" onClick={onRemove}>Remover</button>
                 {/* Remover */}
             </div>
             
